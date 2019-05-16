e82864e39f27ee343e23e0d7ece44ef5af2523a0
Davis JR <davisjr2000@hotmail.com>
2019-05-16 10:01:36 -0300

    Create Gem

---
+++ lib/roberto_barros.rb
@@ -0,0 +1,21 @@
+class RobertoBarros
+  def self.quote
+    return [
+      "Ué?",
+      "Lutador de sumô não limpa a bunda sozinho.",
+      "Eu acho que quem tatua o rosto não entende como a sociedade funciona.",
+      "Mas se esse computador está ruim, se você colocar Windows vai ficar pior ainda!",
+      "Né...",
+      "Não existe projeto simplificado, existe Dev sem imaginação.",
+      "Se o código não é simples, não é Ruby.",
+      "Um site nunca fica pronto. Mas você precisa entregar em algum momento.",
+      "E aí você toma um erro no meio de fuça!",
+      "A pessoa escreve Rubens nos Trilhos e desenha uma rodovia.",
+      "Mas não faz sentido o que você está fazendo!",
+      "Poder, poder, você pode, mas não sei porque você faria isso.",
+      "A cópia é o primeiro passo para a criação.",
+      "Não existem limites para a criatividade do ser humano.",
+      "Está um ótimo dia para programar.",
+    ].sample
+  end
+end
---
+++ roberto_barros.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'roberto_barros'
+  s.version     = '0.0.1'
+  s.date        = '2019-05-16'
+  s.summary     = "Roberto Barros"
+  s.description = "A simple gem that randomly generates quotes from the great Roberto Barros."
+  s.authors     = ["davisjr2000"]
+  s.email       = 'davisrobertosouza@gmail.com'
+  s.files       = ["lib/roberto_barros.rb"]
+  s.homepage    =
+    'https://rubygems.org/gems/roberto_barros'
+  s.license       = 'MIT'
+end
