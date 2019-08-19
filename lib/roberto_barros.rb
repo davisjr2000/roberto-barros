f8ed63025dc8be30216e851e42b9d600a90e19b9
Bruno Parga <brunoparga@gmail.com>
2019-08-19 20:13:23 -0300

    Alphabetize quotes

--- lib/roberto_barros.rb
+++ lib/roberto_barros.rb
@@ -1,28 +1,28 @@
 class RobertoBarros
   def self.quote
     return [
-      "Ué?",
-      "Lutador de sumô não limpa a bunda sozinho.",
-      "Eu acho que quem tatua o rosto não entende como a sociedade funciona.",
-      "Mas se esse computador está ruim, se você colocar Windows vai ficar pior ainda!",
-      "Né...",
-      "Não existe projeto simples, existe Dev sem imaginação.",
-      "Se o código não é simples, não é Ruby.",
-      "Um site nunca fica pronto. Mas você precisa entregar em algum momento.",
-      "E aí você toma um erro no meio de fuça!",
-      "A pessoa escreve Rubens nos Trilhos e desenha uma rodovia.",
-      "Mas não faz sentido o que você está fazendo!",
-      "Poder, poder, você pode, mas não sei porque você faria isso.",
       "A cópia é o primeiro passo para a criação.",
-      "Não existem limites para a criatividade do ser humano.",
+      "A pessoa escreve Rubens nos Trilhos e desenha uma rodovia.",
+      "A vida é muito curta para desperdiçar programando em Java.",
+      "E aí você toma um erro no meio da fuça!",
       "Está um ótimo dia para programar.",
+      "Eu acho que quem tatua o rosto não entende como a sociedade funciona.",
+      "Lutador de sumô não limpa a bunda sozinho.",
+      "Mas não faz sentido o que você está fazendo!",
+      "Mas se esse computador está ruim, se você colocar Windows vai ficar pior ainda!",
+      "Não existe projeto simples, existe dev sem imaginação.",
+      "Não existem limites para a criatividade do ser humano.",
+      "Não faz sentido! Ele quer trocar seis por meia dúzia!!",
+      "Né...",
       "O importante da regra é saber quando quebrar a regra.",
+      "Open Source é a programação mais eficiente que existe.",
+      "Poder, poder, você pode, mas não sei porque você faria isso.",
       "Quando você está programando, seu pior inimigo é você mesmo.",
       "Reboot quem dá é a vida.",
-      "Não faz sentido! Ele quer trocar seis por meia dúzia!!",
       "Scrum dá para aprender em 15 minutos, rsrsrs",
-      "A vida é muito curta para desperdiçar programando em Java.",
-      "Open Source é a programação mais eficiente que existe"
+      "Se o código não é simples, não é Ruby.",
+      "Ué?",
+      "Um site nunca fica pronto. Mas você precisa entregar em algum momento."
     ].sample
   end
 
